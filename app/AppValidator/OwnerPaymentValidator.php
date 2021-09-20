0416197e4497e83ab60490c6d4adac5029988b72
chandraodgroup <76548819+chandraodgroup@users.noreply.github.com>
2021-09-20 11:33:27 +0530

    Owner Payment Report

--- app/AppValidator/OwnerPaymentValidator.php
+++ app/AppValidator/OwnerPaymentValidator.php
@@ -14,6 +14,7 @@ class OwnerPaymentValidator
             'bus_operator_id' => 'required',
             'date' => 'required',
             'amount' => 'required',
+            'transaction_id' => 'required',
             'remark' => 'required',
             'created_by' => 'required'
             
