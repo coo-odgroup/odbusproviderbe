c68df90edc875ec303a8a54044fad5c21f0b30a4
chandraodgroup <76548819+chandraodgroup@users.noreply.github.com>
2021-10-22 14:35:31 +0530

    Page content Repository Updated

--- app/Repositories/PageContentRepository.php
+++ app/Repositories/PageContentRepository.php
@@ -22,13 +22,12 @@ class PageContentRepository
     public function __construct(PageContent $pagecontent )
     {
        $this->pagecontent = $pagecontent ;
-       
+       $this->bus_operator_id = Config::get('constants.BUS_OPERATOR_ID');
     }    
     public function getAll()
     {
 
-        return $this->pagecontent->where('status', 1)->get();
-
+        return $this->pagecontent->where('bus_operator_id',$this->bus_operator_id)->where('status', 1)->get();
     }
 
 
@@ -38,6 +37,7 @@ class PageContentRepository
 
        $pagecontent = new $this->pagecontent;
        $pagecontent->page_name =$data['page_name'];
+       $pagecontent->bus_operator_id = $this->bus_operator_id;
        $pagecontent->page_url =$data['page_url'];
        $pagecontent->page_description =$data['page_description'];
        $pagecontent->meta_title =$data['meta_title'];
@@ -56,8 +56,9 @@ class PageContentRepository
     public function updatepagecontent($data, $id)
     {
     	// Log::info($id);
-       $pagecontent = $this->pagecontent->find($id);
-       $pagecontent->page_name =$data['page_name'];
+      $pagecontent = $this->pagecontent->find($id);
+      $pagecontent->page_name =$data['page_name'];
+      $pagecontent->bus_operator_id = $this->bus_operator_id;
 	   $pagecontent->page_url =$data['page_url'];
 	   $pagecontent->page_description =$data['page_description'];
 	   $pagecontent->meta_title =$data['meta_title'];
@@ -74,7 +75,7 @@ class PageContentRepository
 
     public function deletepagecontent($id)
     {
-    	$pagecontent = $this->pagecontent->find($id);
+    	$pagecontent = $this->pagecontent->where('bus_operator_id',$this->bus_operator_id)->find($id);
     	$pagecontent->status = 2;
     	$pagecontent->update();
 
