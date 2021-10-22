2087e381a3bc2815b21e9135dc8851818f4a6616
chandraodgroup <chandra@odgroup.in>
2021-10-22 13:26:59 +0530

    master setting

--- app/Models/OdbusCharges.php
+++ app/Models/OdbusCharges.php
@@ -11,6 +11,6 @@ class OdbusCharges extends Model
     use HasFactory;
     protected $table = 'odbus_charges';
     protected $fillable = [
-        'payment_gateway_charges','email_sms_charges','odbus_gst_charges','advance_days_show','support_email','booking_email','request_email','other_email','mobile_no_1','mobile_no_2','mobile_no_3','mobile_no_4'
+        'payment_gateway_charges','email_sms_charges','odbus_gst_charges','advance_days_show','support_email','booking_email','request_email','other_email','mobile_no_1','mobile_no_2','mobile_no_3','mobile_no_4','logo'
     ];
 }
