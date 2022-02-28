dc509908cf18147ac90ce2d4a3b2c30a9af5856e
LIMA MOHANTY <chandraodgroup>
2022-02-28 14:13:08 +0530

    changes by Lima

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,11 +17,11 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\Cors::class,
 
         
     ];
@@ -68,7 +68,8 @@ class Kernel extends HttpKernel
 
 
         //'log.route' =>Illuminate\Routing\Middleware\UserController::class
-        'log.route' => \App\Http\Middleware\LogRoute::class
+        'log.route' => \App\Http\Middleware\LogRoute::class,
+        'cors' => \App\Http\Middleware\Cors::class,
         
     ];
 }
