ee5930b297b16605a5a71855a8028017c8a90ad1
LIMA MOHANTY <chandraodgroup>
2026-02-28 15:40:12 +0530

    changes

--- app/Http/Controllers/SeatOpenController.php
+++ app/Http/Controllers/SeatOpenController.php
@@ -1,9 +1,6 @@
-    <?php
-
+<?php
     namespace App\Http\Controllers;
-
     use Illuminate\Http\Request;
-
     use Illuminate\Support\Facades\Validator;
     use InvalidArgumentException;
     use App\Traits\ApiResponser;
@@ -86,8 +83,13 @@
         public function updateSeatOpenData(Request $request)
         {
 
-            $seatopen = $this->seatopenRepository->updateSeatOpenData($request);
-            return $this->successResponse($seatopen, "Seat Open  Added", Response::HTTP_OK);
+            $res = $this->seatopenRepository->updateSeatOpenData($request);
+            if (isset($res['status']) && $res['status'] == 'error') {
+
+                return $this->errorResponse($res['message'], Response::HTTP_OK);
+            } else {
+                return $this->successResponse($res, "Seat Open  Updated", Response::HTTP_OK);
+            }
         }
         public function updateseatopen(Request $request, $id)
         {
