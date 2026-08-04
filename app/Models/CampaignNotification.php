395b25bc1836e81b5dd37ac3bb80e789f26ec199
subhasis mohanty <subhasis.odbus@gmail.com>
2026-08-04 18:18:01 +0530

    campaign notifivation

---
+++ app/Models/CampaignNotification.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class CampaignNotification extends Model
+{
+    use HasFactory;
+
+    protected $table = 'notification_campaigns';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'campaign_name',
+        'title',
+        'message',
+        'image',
+        'type',
+        'target_type',
+        'schedule_type',
+        'schedule_minutes',
+        'schedule_at',
+        'active_status',
+        'total_users',
+        'processed_users',
+        'success_users',
+        'failed_users',
+        'is_completed',
+        'started_at',
+        'completed_at',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+}
